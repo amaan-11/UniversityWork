ccac2e8441c5007b9aa29d9d3a3d8cfee162b277
Amaan <amaan.brs@gmail.com>
2023-08-25 10:37:36 +0300

    init commit

---
+++ fourthexercise.py
@@ -0,0 +1,7 @@
+a=int(input("Enter an integer"))
+b=int(input("Enter an integer"))
+c=int(input("Enter an integer"))
+sum=a+b+c
+product=a*b*c
+avg=sum/3
+print("The sum of the three numbers =",sum,"\nThe product of the three numbers =",product,"\nThe average of the three numbers =",avg)
---
+++ thirdexercise.py
@@ -0,0 +1,5 @@
+length=float(input("Enter the length of the rectangle,"))
+breadth=float(input("Enter the breadth of the rectangle,"))
+perimeter=2*(length+breadth)
+area=length*breadth
+print("The perimeter of the rectangle =",perimeter,"\nThe area of the rectangle =",area)
