9f8a59f2c04a72a0ac0a656186b9941216b5081f
Amaan <amaan.brs@gmail.com>
2023-08-26 21:48:20 +0300

    init commit

---
+++ csfourthexercise.py
@@ -0,0 +1,7 @@
+year=int(input("Enter a year"))
+if year%4==0:
+    if year%100==0:
+        if year%400:
+            print("The year "+str(year)+" is a leap year")
+    else:
+        print("The year "+str(year)+" is a leap year")
